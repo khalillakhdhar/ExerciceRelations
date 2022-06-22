fb7616ed2aa05eebeee8f37c4cebab74e396afff
khalil <khalillakhdharatc@gmail.com>
2022-06-22 08:47:10 +0100

    update

--- src/main/java/com/doranco/relations/model/Categorie.java
+++ src/main/java/com/doranco/relations/model/Categorie.java
@@ -1,5 +1,34 @@
 package com.doranco.relations.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categorie {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false,unique = true)
+	
+	private String titre;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
+	
+	
+	
 }
--- src/main/java/com/doranco/relations/model/Produit.java
+++ src/main/java/com/doranco/relations/model/Produit.java
@@ -1,5 +1,94 @@
 package com.doranco.relations.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Produit {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	
+	private String titre;
+@Column(nullable = false)
+	
+	private String photo;
+@Column(nullable = false)
+
+private String description;
+@Column(nullable = false)
+
+private float prix;
+@Column(nullable = false)
+
+private int id_user;
+
+@ManyToOne(optional = false)
+@JoinColumn(name = "categorie",referencedColumnName = "titre")
+private Categorie categorie;
+
+public long getId() {
+	return id;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+
+public String getTitre() {
+	return titre;
+}
+
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+
+public String getPhoto() {
+	return photo;
+}
+
+public void setPhoto(String photo) {
+	this.photo = photo;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public float getPrix() {
+	return prix;
+}
+
+public void setPrix(float prix) {
+	this.prix = prix;
+}
+
+public int getId_user() {
+	return id_user;
+}
+
+public void setId_user(int id_user) {
+	this.id_user = id_user;
+}
+
+public Categorie getCategorie() {
+	return categorie;
+}
+
+public void setCategorie(Categorie categorie) {
+	this.categorie = categorie;
+}
+
 
 }
